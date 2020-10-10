b236f96b3f3e0d3bceb0c4807d1f4c05164281e9
firiontinaa <firiontina.argan.tif17@polban.ac.id>
2020-10-10 19:36:28 +0700

    add icon weather

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodayComponent } from './today/today.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, TodayComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatAutocompleteModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/weather.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WeatherService {
+  url = 'http://api.openweathermap.org/data/2.5';
+  apiKey = '6750f627afa043d7eb1677a5b46002d1';
+
+  constructor(private http: HttpClient) {}
+
+  getWeatherDataByLocation(lat, lon) {
+    let params = new HttpParams()
+      .set('lat', lat)
+      .set('lon', lon)
+      .set('units', 'metric')
+      .set('appId', this.apiKey);
+
+    return this.http.get(`${this.url}/weather`, { params });
+  }
+
+  getWeatherDataByCity(city: string) {
+    let params = new HttpParams()
+      .set('q', city)
+      .set('units', 'metric')
+      .set('appId', this.apiKey);
+
+    return this.http.get(`${this.url}/weather`, { params });
+  }
+
+  getForecastDataByLocation(lat, lon) {
+    let params = new HttpParams()
+      .set('lat', lat)
+      .set('lon', lon)
+      .set('cnt', '16')
+      .set('units', 'metric')
+      .set('appId', this.apiKey);
+
+    return this.http.get(`${this.url}/forecast`, { params });
+  }
+
+  getForecastDataByCity(city: string) {
+    let params = new HttpParams()
+      .set('q', city)
+      .set('cnt', '16')
+      .set('units', 'metric')
+      .set('appId', this.apiKey);
+
+    return this.http.get(`${this.url}/forecast`, { params });
+  }
+
+  public getCityList(): Observable<any> {
+    return this.http.get('./assets/city.list.json');
+  }
+}
